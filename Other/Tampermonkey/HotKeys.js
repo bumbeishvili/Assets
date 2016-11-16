1d0915cb29b459dde3743424946ba46e97017c0f
Dato Bumbeishvili <bumbeishvili@users.noreply.github.com>
2016-11-16 21:47:24 +0400

    added password  input revealing

--- Other/Tampermonkey/HotKeys.js
+++ Other/Tampermonkey/HotKeys.js
@@ -12,22 +12,30 @@
 (function() {
     'use strict';
 
-
-
+    
+  var keyCodes =   {"A":65,"B":66,"C":67,"D":68,"E":69,"F":70,"G":71,"H":72,"I":73,"J":74,"K":75,
+                    "L":76,"M":77,"N":78,"O":79,"P":80,"Q":81,"R":82,"S":83,"T":84,"U":85,"V":86,
+                    "W":87,"X":88,"Y":89,"Z":90}
 
     document.addEventListener("keydown", event=>{
 
-        if( event.which == 85 && event.altKey ) {
+        if( event.which == keyCodes.U && event.altKey ) {
             console.log('you pressed altKey U - unblock' );
             enableDisabledElements();
 
         }
 
-        if( event.which == 67 && event.altKey ) {
+        if( event.which == keyCodes.C && event.altKey ) {
             console.log('you pressed altKey C - contentEditable = true' );
             document.body.contentEditable = true;
 
         }
+        
+        if( event.which == keyCodes.P && event.altKey){
+          console.log('you pressed altKey P - show passwords ');
+          revealSavedHiddenPasswords(); 
+        }
+    
         if( event.which && event.altKey ) {
             console.log('you pressed altKey ' + event.which );
         }
@@ -35,7 +43,9 @@
     }, false);
 
 
-
+    function revealSavedHiddenPasswords(){
+        document.querySelectorAll('input[type=password]').forEach(v=>v.type='text');
+    }
 
     function enableDisabledElements () {
         var selectors = ['button', 'input', 'textarea', 'select', 'li', 'a', 'fieldset', 'div', 'img'];
@@ -48,5 +58,7 @@
             });
         });
     }
-
+    
+    
+   
 })();
