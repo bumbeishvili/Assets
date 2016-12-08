658e2126f1c5d9eb676f1eac7127355b89a13ee5
Dato Bumbeishvili <bumbeishvili@users.noreply.github.com>
2016-12-09 03:06:57 +0400

    added guid generation

--- Other/Tampermonkey/HotKeys.js
+++ Other/Tampermonkey/HotKeys.js
@@ -1,4 +1,3 @@
-
 // ==UserScript==
 // @name         Hot Keys
 // @namespace    http://tampermonkey.net/
@@ -6,16 +5,18 @@
 // @description  try to take over the world!
 // @author       You
 // @match         *://*/*
-// @grant        none
+// @grant GM_setClipboard
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    
-  var keyCodes =   {"A":65,"B":66,"C":67,"D":68,"E":69,"F":70,"G":71,"H":72,"I":73,"J":74,"K":75,
-                    "L":76,"M":77,"N":78,"O":79,"P":80,"Q":81,"R":82,"S":83,"T":84,"U":85,"V":86,
-                    "W":87,"X":88,"Y":89,"Z":90};a
+
+    var keyCodes =   {"A":65,"B":66,"C":67,"D":68,"E":69,"F":70,"G":71,"H":72,"I":73,"J":74,"K":75,
+                      "L":76,"M":77,"N":78,"O":79,"P":80,"Q":81,"R":82,"S":83,"T":84,"U":85,"V":86,
+                      "W":87,"X":88,"Y":89,"Z":90};
+
+
 
     document.addEventListener("keydown", event=>{
 
@@ -30,17 +31,60 @@
             document.body.contentEditable = true;
 
         }
-        
+
         if( event.which == keyCodes.P && event.altKey){
-          console.log('you pressed altKey P - show passwords ');
-          revealSavedHiddenPasswords(); 
+            console.log('you pressed altKey P - show passwords ');
+            revealSavedHiddenPasswords(); 
         }
-    
+
+        if( event.which == keyCodes.G && event.altKey){
+            console.log('you pressed altKey G - pretify guids ');
+            addTextField();
+        }
+
         if( event.which && event.altKey ) {
             console.log('you pressed altKey ' + event.which );
         }
 
     }, false);
+
+    function addTextField(){
+        if(!document.querySelector('.guidFormating')){
+            var div = get("body");
+            var input = document.createElement("textarea");
+            var button = document.createElement("button");
+            input.name = "post";
+            input.maxLength = "500";
+            input.cols = "20";
+            input.rows = "5";
+            input.classList.add("guidFormatting");
+            div.insertBefore(input, div.firstChild);
+            var oldVal = "";
+            setInterval(function(){
+                var newval =  get(".guidFormatting").value;
+                if(oldVal!=newval && newval.trim()){
+                    oldVal = newval;
+                    replaceRawToFormatted();
+                }
+            },100);
+        }
+
+    }
+
+    function replaceRawToFormatted() {
+        var value = get(".guidFormatting").value;
+        var guids = value.split('\n');
+        var mapped = guids.map(function(v, i) {
+            var result = "'" + v + "'";
+            if (i != guids.length - 1) {
+                result += ',\n';
+            }
+            return result;
+        });
+
+        mapped = mapped.join("");
+        GM_setClipboard (mapped);
+    }
 
 
     function revealSavedHiddenPasswords(){
@@ -59,6 +103,10 @@
         });
     }
     
-    
-   
+    function get(selector){
+        return document.querySelector(selector);
+    }
+
+
+
 })();
